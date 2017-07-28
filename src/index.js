fcfd7e8eb545e69b00f6cfadc67cf4f01ca77057
dianjiangtan <806454399@qq.com>
2017-07-28 18:35:23 +0800

    xinzheng

--- src/index.js
+++ src/index.js
@@ -2,13 +2,44 @@
  * Created by TC on 2017/7/27.
  */
 import _ from "lodash"
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    // var btn = document.createElement('button');
 
+    // element.innerHTML = _.join(['Hello', 'webapack'], ' ');
+
+    // btn.innerHTML = 'Click me and check the console!';
+    // btn.onclick = printMe;
+    //
+    // element.appendChild(btn);
+    var button = document.createElement('button');
+    var br = document.createElement('br');
+
+    button.innerHTML = 'Click me and look at the console!';
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.appendChild(br);
+    element.appendChild(button);
+
+    // Note that because a network request is involved, some indication
+    // of loading would need to be shown in a production-level site/app.
+    button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+        var print = module.default;
+
+        print();
+    });
 
     return element;
 }
-
 document.body.appendChild(component());
+
+/*import Library from './library';
+ // import './styles.css';
+ if (module.hot) {
+ module.hot.accept('./library', function() {
+ console.log('Accepting the updated library module!');
+ Library.log();
+ })
+ }*/
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,50 @@
 /**
  * Created by TC on 2017/7/27.
  */
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const WebpackManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.css$/,
+    //             use: [ 'style-loader', 'css-loader' ]
+    //         }
+    //     ]
+    // },
+    // devtool: 'inline-source-map',
+    // devServer: {
+    //     hot: true, // 告诉 dev-server 我们在使用 HMR
+    //     contentBase: path.resolve(__dirname, 'dist'),
+    //     publicPath: '/'
+    // },
+    plugins: [
+        // new webpack.HotModuleReplacementPlugin(), // 启用 HMR
+        // new CleanWebpackPlugin(['dist']),
+        // new WebpackManifestPlugin({
+        //     // fileName: 'manifest.json',
+        //     // basePath: '/dist/',
+        //     // seed: {
+        //     //     name: 'My Manifest'
+        //     // }
+        // }),
+        // new HtmlWebpackPlugin({
+        //     title: 'Output Management'
+        // }),
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'common' // Specify the common bundle's name.
+        // })
+    ],
 };
